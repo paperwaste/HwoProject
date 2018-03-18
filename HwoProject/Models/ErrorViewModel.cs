64cc36ac4b2d2be6cf2993800ca12079ad49ad8a
paperwaste <paperwaste92@gmail.com>
2018-03-18 22:03:38 +1100

    HelloWorld
    Initial Commit

---
+++ HwoProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HwoProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
